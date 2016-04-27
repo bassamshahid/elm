d398a7973153d717312292953086e695b899df95
zeeshan <anonymousguyx@gmail.com>
2016-04-28 00:34:39 +0500

    Database File Added
    Database folder and a file to create connection with database has been created.

---
+++ db/db.php
@@ -0,0 +1,14 @@
+<?php 
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "elm";
+
+	//Create Connection
+	$conn = mysql_connect($servername, $username, $password, $dbname);
+
+	//Check Connection if die
+	if (!$conn) {
+		die("Failed to connect with database.")
+	}
+ ?>
